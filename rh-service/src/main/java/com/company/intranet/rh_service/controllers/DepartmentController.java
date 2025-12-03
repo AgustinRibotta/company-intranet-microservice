a847849a29559117c2e7de2a33db93e334a2f15e
Agustin Ribotta <agustin.ribotta.r@gmail.com>
2025-12-03 20:29:40 +0100

    Generate Department Controllers

---
+++ rh-service/src/main/java/com/company/intranet/rh_service/controllers/DepartmentController.java
@@ -0,0 +1,55 @@
+package com.company.intranet.rh_service.controllers;
+
+import com.company.intranet.rh_service.dtos.request.DepartmentRequestDto;
+import com.company.intranet.rh_service.dtos.response.DepartmentResponseDto;
+import com.company.intranet.rh_service.services.DepartmentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/rh/departments")
+public class DepartmentController {
+
+    private final DepartmentService service;
+
+    public DepartmentController(DepartmentService service) {
+        this.service = service;
+    }
+
+//    @PreAuthorize("hasAuthority('READ_DP')")
+    @GetMapping
+    public ResponseEntity<List<DepartmentResponseDto>> findAll(){
+        return ResponseEntity.ok().body(this.service.findAll());
+    }
+
+//    @PreAuthorize("hasAuthority('READ_DP')")
+    @GetMapping("/department/{id}")
+    public ResponseEntity<DepartmentResponseDto> findById(@PathVariable UUID id){
+        return ResponseEntity.ok().body(this.service.findById(id));
+    }
+
+//    @PreAuthorize("hasAuthority('CREATE_DP')")
+    @PostMapping()
+    public ResponseEntity<DepartmentResponseDto> save (@Validated @RequestBody DepartmentRequestDto dto) {
+        DepartmentResponseDto newD = this.service.save(dto);
+        URI location = URI.create("/rh/departments/" + newD.getId());
+        return ResponseEntity.created(location).body(newD);
+    }
+
+//    @PreAuthorize("hasAuthority('UPDATE_DP')")
+    @PutMapping("/department/{id}")
+    public ResponseEntity<DepartmentResponseDto> update (@RequestBody DepartmentRequestDto dto, @PathVariable UUID id) {
+        return ResponseEntity.ok().body(this.service.update(dto, id));
+    }
+
+//    @PreAuthorize("hasAuthority('DELETE_DP')")
+    public ResponseEntity<?> delete (@PathVariable UUID id){
+        this.service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- rh-service/src/main/java/com/company/intranet/rh_service/entities/DepartmentEntity.java
+++ rh-service/src/main/java/com/company/intranet/rh_service/entities/DepartmentEntity.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "department_entitye")
+@Table(name = "department_entity")
 public class DepartmentEntity {
 
     @Id
